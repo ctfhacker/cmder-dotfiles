ff052361c5644ea84ba3922115fa8654bb33078b
thebarbershopper <cld251@gmail.com>
2017-01-18 14:04:08 -0600

    Don't use registry and use XML file instead for cmder

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 copy user-aliases.cmd C:\tools\cmder\config\
 move vimfiles ../.vim
-reg import ConEmu.reg
+mkdir C:\tools\cmder\vendor\conemu-maximus5
+move ConEmu.xml C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml
