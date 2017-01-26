09914ee4717dd858cc4864df2d342298617b4e04
thebarbershopper <cld251@gmail.com>
2017-01-26 09:23:00 -0600

    Use regitry for importing settings

--- install.bat
+++ install.bat
@@ -1,5 +1,4 @@
 copy user-aliases.cmd C:\tools\cmder\config\
 copy vimfiles/vimrc ../_vimrc
 move vimfiles ../.vim
-mkdir C:\tools\cmder\vendor\conemu-maximus5
-move ConEmu.xml C:\tools\cmder\vendor\conemu-maximus5\ConEmu.xml
+reg import ConEmu.reg
